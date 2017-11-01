9bc3e11169ca934b3cffaa069a31e34af0fb877c
mszewczyk <szewczykm94@gmail.com>
2017-11-01 12:04:38 +0100

    Extracted tuple

--- src/main/java/com/huffman/coding/utils/ValueWithFrequency.java
+++ src/main/java/com/huffman/coding/utils/ValueWithFrequency.java
@@ -1,19 +1,17 @@
 package com.huffman.coding.utils;
 
 public final class ValueWithFrequency<V> {
-    private final V value;
-    private final Integer frequency;
+    private final Tuple<V, Integer> valueWithFrequency;
 
     public ValueWithFrequency(V value, Integer frequency) {
-        this.value = value;
-        this.frequency = frequency;
+        valueWithFrequency = new Tuple<>(value, frequency);
     }
 
     public V getValue() {
-        return value;
+        return valueWithFrequency.getKey();
     }
 
     public Integer getFrequency() {
-        return frequency;
+        return valueWithFrequency.getValue();
     }
 }
